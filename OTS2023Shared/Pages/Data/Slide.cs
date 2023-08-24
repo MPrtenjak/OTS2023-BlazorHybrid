410b9c4b92706e5128c9651dc50e513cef537357
Matjaz Prtenjak <matjaz.prtenjak@endava.com>
2023-08-24 10:44:19 +0200

    Still cleaning code from OTS 2022

--- OTS2023Shared/Pages/Data/Slide.cs
+++ OTS2023Shared/Pages/Data/Slide.cs
@@ -14,24 +14,7 @@ namespace OTS2023Shared.Pages.Data
             content: xElement.Element("Content")!.Value.Trim()
         );
 
-      if (string.Equals(type, "Comparer", StringComparison.InvariantCultureIgnoreCase))
-        return new ComparerSlideDTO(
-            caption: xElement.Element("Caption")!.Value,
-            vueCode: xElement.Element("VueCode")?.Value.Trim() ?? string.Empty,
-            blazorCode: xElement.Element("BlazorCode")?.Value.Trim() ?? string.Empty,
-            blazorComponent: GetTypeFromPartialName(xElement.Element("BlazorComponent")!.Value)
-        );
-
-      return new ProjectFileSlideDTO(
-          caption: xElement.Element("Caption")!.Value,
-          content: xElement.Element("Content")!.Value.Trim()
-      );
-    }
-
-    private static Type GetTypeFromPartialName(string partialName)
-    {
-      var fullTypeName = "OTS2023Shared.Pages.Components." + partialName;
-      return Type.GetType(fullTypeName)!;
+      throw new ArgumentException($"Unknown slide type: {type}");
     }
   }
 }
